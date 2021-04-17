4d497c640c1e09386ae3839e8fe6deca7932004e
John Teckemeyer <johnteckemeyer@gmail.com>
2021-04-16 20:01:49 -0700

    Display a 'firefly' square of random size and location

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import Display from "../Display/Display";
+import { useEffect, useState } from 'react';
 
 function App() {
+  // Component State
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  const [fireflies, setFireflies] = useState([]);
+
+  // Update the width and height state of display
+  function updateSize () {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  function updateFireflies () {
+    // temporary creation of a firefly
+    setFireflies([
+      ...fireflies,
+      {
+        x: Math.floor(Math.random() * window.innerWidth),
+        y: Math.floor(Math.random() * window.innerHeight),
+        size: Math.floor(Math.random() * 100) + 100
+      }
+    ])
+  }
+
+  // Listen for any changes to display size
+  useEffect(() => {
+    window.addEventListener('resize', updateSize);
+    updateFireflies();
+  }, []);
+
   return (
     <div className="App">
-      <Display />
+      <h1>{width} X {height}</h1>
+      <Display fireflies={fireflies} />
     </div>
   );
 }
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./Display.css";
 
-export default function Display () {
+export default function Display ({ fireflies }) {
+
     return (
         <div className="display">
             <h1>This is the display</h1>
+            {
+                fireflies.map((firefly, index) => {
+                    return <div className="firefly" style={{ top: firefly.y, left: firefly.x, width: firefly.size, height: firefly.size }}></div>
+                })
+            }
         </div>
     )
 }
